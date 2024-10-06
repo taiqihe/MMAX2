6e35441579860c6aa12ce12d1cb3093a58b9caa8
taiqihe <htq00007@gmail.com>
2024-10-05 22:46:26 -0400

    batch update copyright notice to fix unicode character

--- Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAXFileFilter.java
+++ Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAXFileFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Mark-Christoph M�ller
+ * Copyright 2021 Mark-Christoph Müller
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
